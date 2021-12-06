c8ccd41aebeb1fa0ca5e28ca2a5a693edd85b1a2
eswaribala <parameswaribala@gmail.com>
2021-12-06 13:36:22 +0530

    updated

---
+++ customerdataapi/src/main/java/com/boa/customerdataapi/CustomerdataapiApplication.java
@@ -0,0 +1,13 @@
+package com.boa.customerdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdataapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdataapiApplication.class, args);
+	}
+
+}
