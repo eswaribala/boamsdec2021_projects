38acefd717114c9296032ddfc2a6d40c02eeaba9
eswaribala <parameswaribala@gmail.com>
2021-12-07 13:05:32 +0530

    updated

--- customerdatacloudapi/src/main/java/com/boa/customerdataapi/controllers/CustomerController.java
+++ customerdatacloudapi/src/main/java/com/boa/customerdataapi/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package com.boa.customerdataapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,12 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.boa.customerdataapi.models.Customer;
 import com.boa.customerdataapi.services.CustomerService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/customers")
+@RefreshScope
+@Slf4j
 public class CustomerController {
     @Autowired
 	private CustomerService customerService;    
-    
+    @Value("${message}")
+    private String message;
     @PostMapping({"/v1.0", "/v1.1"})
     public ResponseEntity<?> addCustomer(@RequestBody Customer customer){
     	
@@ -33,6 +40,8 @@ public class CustomerController {
     }
     @GetMapping({"/v1.0", "/v1.1"})
     public List<Customer> fetchAllCustomers(){
+    	//test message change
+    	log.info("Message Modified in Github"+message);
     	return this.customerService.getAllCustomers();
     }
     
